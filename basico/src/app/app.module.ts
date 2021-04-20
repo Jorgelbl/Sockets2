b24c40f617b15b9bf0d465d66c25773a538d4f7b
Jorge Luis <47648890+Jorgelbl@users.noreply.github.com>
2021-04-20 12:55:10 -0500

    Se agrego la carpeta basico

---
+++ basico/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+//Sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { 
+  url: environment.wsURL, options: {} };
+
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    SocketIoModule.forRoot(config)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
